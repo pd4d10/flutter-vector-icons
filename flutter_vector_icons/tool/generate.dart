f39ea95321050e5135f4eab413f3edbab132ccc3
Rongjian Zhang <pd4d10@gmail.com>
2019-07-16 22:19:40 +0800

    build: update tool to generate web resources

--- flutter_vector_icons/tool/generate.dart
+++ flutter_vector_icons/tool/generate.dart
@@ -4,17 +4,23 @@ import 'package:dart_style/dart_style.dart';
 
 var specialChars = ['new', 'sync', 'switch', 'try', 'null', 'class'];
 
+// * Replace - with _
+// * Language reserved words: new -> new_icon
+// * Key starts with number: 500px -> icon_500px
+String getKey(String key) {
+  key = key.replaceAll('-', '_');
+  if (specialChars.contains(key)) {
+    key = key + '_icon';
+  }
+  if (key.startsWith(RegExp(r'\d'))) {
+    key = 'icon_' + key;
+  }
+  return key;
+}
+
 String convert(String name, Map input) {
   var properties = input.entries.map((entry) {
-    String key = entry.key.replaceAll('-', '_');
-    if (specialChars.contains(key)) {
-      key = key + '_icon';
-    }
-
-    if (key.startsWith(RegExp(r'\d'))) {
-      key = 'icon_' + key;
-    }
-
+    var key = getKey(entry.key);
     var value = entry.value;
     return 'static const IconData $key = IconData($value, fontFamily: "$name", fontPackage: "flutter_vector_icons");';
   }).join('\n');
@@ -83,4 +89,27 @@ $exports
 ''';
   File('./lib/flutter_vector_icons.dart')
       .writeAsStringSync(DartFormatter().format(result));
+
+  // web project
+  Map webData = {};
+  List fontManifest = [];
+  names.forEach((name) {
+    var content = File('./glyphmaps/$name.json').readAsStringSync();
+    Map input = json.decode(content);
+    webData[name] = Map.fromEntries(
+        input.entries.map((entry) => MapEntry(getKey(entry.key), entry.value)));
+    fontManifest.add({
+      "family": name,
+      "fonts": [
+        {"asset": "fonts/$name.ttf"}
+      ]
+    });
+  });
+
+  var content = 'var data =' + json.encode(webData) + ';';
+  File('../flutter_vector_icons_gallery/lib/data.dart')
+      .writeAsStringSync(DartFormatter().format(content));
+
+  File('../flutter_vector_icons_gallery/web/assets/FontManifest.json')
+      .writeAsStringSync(json.encode(fontManifest));
 }
