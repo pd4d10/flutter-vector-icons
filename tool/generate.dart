498704e9fc88be7d37462a4c14977a2bef017b3c
Leonardo Louro Justino <leojustino@gmail.com>
2020-03-30 16:07:18 -0300

    generalyze dartFormatter; refactor to standard dart style; use string buffer instead string concatenation; change var to final where it wont change; change bash command when it is windows; generate field values as array with all other values of a class, like an enum;

--- tool/generate.dart
+++ tool/generate.dart
@@ -3,63 +3,33 @@ import 'dart:convert';
 import 'package:path/path.dart' as path;
 import 'package:dart_style/dart_style.dart';
 
-var reservedWords = ['new', 'sync', 'switch', 'try', 'null', 'class'];
+final reservedWords = ['new', 'sync', 'switch', 'try', 'null', 'class'];
+final fontNames = ['AntDesign', 'Entypo', 'EvilIcons', 'Feather', 'FontAwesome', 'Fontisto', 'Foundation', 'Ionicons', 'MaterialCommunityIcons', 'MaterialIcons', 'Octicons', 'SimpleLineIcons', 'Zocial'];
+final fa5FontNames = ['FontAwesome5_Brands', 'FontAwesome5_Regular', 'FontAwesome5_Solid'];
+final root = path.join(path.dirname(Platform.isWindows ? Platform.script.path.replaceFirst('/', '') : Platform.script.path), '..');
+final dartFormatter = DartFormatter();
 
-var root = path.join(path.dirname(Platform.script.path), '..');
-
-var fontNames = [
-  'AntDesign',
-  'Entypo',
-  'EvilIcons',
-  'Feather',
-  'FontAwesome',
-  'Fontisto',
-  'Foundation',
-  'Ionicons',
-  'MaterialCommunityIcons',
-  'MaterialIcons',
-  'Octicons',
-  'SimpleLineIcons',
-  'Zocial'
-];
-
-var fa5FontNames = [
-  'FontAwesome5_Brands',
-  'FontAwesome5_Regular',
-  'FontAwesome5_Solid'
-];
-
-String getAbsolutePath(String filePath) {
-  return path.normalize(path.join(root, filePath));
-}
+String getAbsolutePath(String filePath) => path.normalize(path.join(root, filePath));
 
 // * Replace - with _
 // * Language reserved words: new -> new_icon
 // * Key starts with number: 500px -> icon_500px
 String normalizeKey(String key) {
   key = key.replaceAll('-', '_');
-  var _key = key;
-  if (reservedWords.contains(key)) {
-    key = key + '_icon';
-  }
-  if (key.startsWith(RegExp(r'\d'))) {
-    key = 'icon_' + key;
-  }
-  if (key != _key) {
-    print('[name conversion]: $_key -> $key');
-  }
+  final _key = key;
+
+  if (reservedWords.contains(key)) key = key + '_icon';
+  if (key.startsWith(RegExp(r'\d'))) key = 'icon_' + key;
+  if (key != _key) print('[name conversion]: $_key -> $key');
+
   return key;
 }
 
 // FontAwesome5_Brands -> brands
-String getFa5FontMapKey(String name) {
-  return name.split('_')[1].toLowerCase();
-}
+String getFa5FontMapKey(String name) => name.split('_')[1].toLowerCase();
 
 // FontAwesome5_Brands -> FontAwesome5Brands
-String toClassName(String name) {
-  return name.replaceAll('_', '');
-}
+String toClassName(String name) => name.replaceAll('_', '');
 
 // AntDesign -> ant_design
 // FontAwesome5_Brands -> font_awesome_5_brands
@@ -75,141 +45,142 @@ String toFileName(String input) {
 }
 
 runCommand(String command, {String workingDirectory}) {
-  var res = Process.runSync('/bin/bash', ['-c', command],
-      workingDirectory: workingDirectory);
+  final bashCommand = Platform.isWindows ? 'cmd' : '/bin/bash';
+  final res = Process.runSync(bashCommand, ['-c', command], workingDirectory: workingDirectory);
+
   print(res.stdout);
   print(res.stderr);
 }
 
 generateFonts() {
   // lib/flutter_vector_icons.dart
-  var entryCode = 'library flutter_vector_icons;';
-  for (var name in [...fontNames, ...fa5FontNames]) {
-    var fileName = toFileName(name);
-    entryCode += "export 'src/$fileName.dart';";
-  }
-  File(getAbsolutePath('lib/flutter_vector_icons.dart'))
-      .writeAsStringSync(DartFormatter().format(entryCode));
+  final sourceCode = StringBuffer('library flutter_vector_icons;');
+  final valuesContent = StringBuffer();
+
+  for (var fontName in [...fontNames, ...fa5FontNames]) sourceCode.write("export 'src/${toFileName(fontName)}.dart';");
+
+  File(getAbsolutePath('lib/flutter_vector_icons.dart')).writeAsStringSync(dartFormatter.format(sourceCode.toString()));
 
   // lib/src/*.dart
   // fonts
-  for (var name in fontNames) {
-    var iconMap = json.decode(File(getAbsolutePath(
-            'vendor/react-native-vector-icons/glyphmaps/$name.json'))
-        .readAsStringSync()) as Map;
 
-    var code = '''import 'package:flutter/widgets.dart'; class $name {''';
-    for (var e in iconMap.entries) {
-      var key = normalizeKey(e.key);
-      var value = e.value;
-      code +=
-          'static const IconData $key = IconData($value, fontFamily: "$name", fontPackage: "flutter_vector_icons");';
+  for (final fontName in fontNames) {
+    final fileName = toFileName(fontName);
+    final iconMap = json.decode(File(getAbsolutePath('vendor/react-native-vector-icons/glyphmaps/$fontName.json')).readAsStringSync()) as Map;
+
+    sourceCode.clear();
+    valuesContent.clear();
+    sourceCode.write("import 'package:flutter/widgets.dart'; class $fontName {");
+    valuesContent.write('static const values = {');
+
+    for (var entry in iconMap.entries) {
+      final identifier = normalizeKey(entry.key);
+      final codePoint = entry.value;
+
+      sourceCode.write('static const IconData $identifier = IconData($codePoint, fontFamily: "$fontName", fontPackage: "flutter_vector_icons");');
+      valuesContent.write('"$identifier":$identifier,');
     }
-    code += '}';
 
-    var fileName = toFileName(name);
-    File(getAbsolutePath('lib/src/$fileName.dart'))
-        .writeAsStringSync(DartFormatter().format(code));
-    print('$name done');
+    sourceCode.write(valuesContent);
+    sourceCode.write('};}');
+
+    File(getAbsolutePath('lib/src/$fileName.dart')).writeAsStringSync(dartFormatter.format(sourceCode.toString()));
+
+    print('$fontName done');
   }
 
   // font awesome 5
-  var fa5Meta = json.decode(File(getAbsolutePath(
-          'vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free_meta.json'))
-      .readAsStringSync());
-  var fa5GlyphMap = json.decode(File(getAbsolutePath(
-          'vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free.json'))
-      .readAsStringSync());
+  final fa5Meta = json.decode(File(getAbsolutePath('vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free_meta.json')).readAsStringSync());
+  final fa5GlyphMap = json.decode(File(getAbsolutePath('vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free.json')).readAsStringSync());
 
-  for (var name in fa5FontNames) {
-    var className = toClassName(name);
-    var code = '''import 'package:flutter/widgets.dart'; class $className {''';
+  for (var fontName in fa5FontNames) {
+    final fileName = toFileName(fontName);
+    final className = toClassName(fontName);
 
-    for (var key in fa5Meta[getFa5FontMapKey(name)]) {
-      var nKey = normalizeKey(key);
-      var codePoint = fa5GlyphMap[key];
-      if (codePoint == null) {
-        throw 'codePoint null: $name, $key';
-      }
-      code +=
-          'static const IconData $nKey = IconData($codePoint, fontFamily: "$name", fontPackage: "flutter_vector_icons");';
+    valuesContent.clear();
+    sourceCode.clear();
+    sourceCode.write("import 'package:flutter/widgets.dart'; class $className {");
+    valuesContent.write('static const values = {');
+
+    for (final key in fa5Meta[getFa5FontMapKey(fontName)]) {
+      final identifier = normalizeKey(key);
+      final codePoint = fa5GlyphMap[key];
+
+      if (codePoint == null) throw 'codePoint null: $fontName, $key';
+
+      sourceCode.write('static const IconData $identifier = IconData($codePoint, fontFamily: "$fontName", fontPackage: "flutter_vector_icons");');
+      valuesContent.write('"$identifier":$identifier,');
     }
-    code += '}';
 
-    var fileName = toFileName(name);
-    File(getAbsolutePath('lib/src/$fileName.dart'))
-        .writeAsStringSync(DartFormatter().format(code));
-    print('$name done');
+    sourceCode.write(valuesContent);
+    sourceCode.write('};}');
+
+    File(getAbsolutePath('lib/src/$fileName.dart')).writeAsStringSync(dartFormatter.format(sourceCode.toString()));
+
+    print('$fontName done');
   }
 }
 
 generateWebData() {
   // data.dart
-  Map webData = {};
-  for (var name in fontNames) {
-    var glyphMap = json.decode(File(getAbsolutePath(
-            'vendor/react-native-vector-icons/glyphmaps/$name.json'))
-        .readAsStringSync()) as Map;
-    var result = {};
-    for (var e in glyphMap.entries) {
-      result[normalizeKey(e.key)] = e.value;
-    }
+  final webData = {};
+
+  for (final name in fontNames) {
+    final glyphMap = json.decode(File(getAbsolutePath('vendor/react-native-vector-icons/glyphmaps/$name.json')).readAsStringSync()) as Map;
+    final result = {};
+
+    for (final e in glyphMap.entries) result[normalizeKey(e.key)] = e.value;
+
     webData[name] = result;
   }
 
-  var fa5Meta = json.decode(File(getAbsolutePath(
-          'vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free_meta.json'))
-      .readAsStringSync()) as Map;
-  var fa5GlyphMap = json.decode(File(getAbsolutePath(
-          'vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free.json'))
-      .readAsStringSync()) as Map;
-  for (var name in fa5FontNames) {
-    var result = {};
-    for (var key in fa5Meta[getFa5FontMapKey(name)]) {
-      result[normalizeKey(key)] = fa5GlyphMap[key];
-    }
+  final fa5Meta = json.decode(File(getAbsolutePath('vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free_meta.json')).readAsStringSync()) as Map;
+  final fa5GlyphMap = json.decode(File(getAbsolutePath('vendor/react-native-vector-icons/glyphmaps/FontAwesome5Free.json')).readAsStringSync()) as Map;
+
+  for (final name in fa5FontNames) {
+    final result = {};
+
+    for (final key in fa5Meta[getFa5FontMapKey(name)]) result[normalizeKey(key)] = fa5GlyphMap[key];
+
     webData[name] = result;
   }
 
-  File(getAbsolutePath('example/lib/data.dart')).writeAsStringSync(
-      DartFormatter().format('var data =' + json.encode(webData) + ';'));
+  File(getAbsolutePath('example/lib/data.dart')).writeAsStringSync(dartFormatter.format('var data =${json.encode(webData)};'));
 
   // FontManifest.json
-  var fontManifest = [...fontNames, ...fa5FontNames].map((name) {
-    return {
-      "family": name,
-      "fonts": [
-        {"asset": "fonts/$name.ttf"}
-      ]
-    };
-  }).toList();
+  final fontManifest = [...fontNames, ...fa5FontNames]
+      .map((name) => {
+            "family": name,
+            "fonts": [
+              {"asset": "fonts/$name.ttf"}
+            ]
+          })
+      .toList();
 
   // pubspec.yaml
-  var pubspecString = File(getAbsolutePath('pubspec.yaml')).readAsStringSync();
-  var l = pubspecString.split('''flutter:
-  fonts:
-''');
-  l[1] = '';
-  for (var item in fontManifest) {
-    var family = item['family'];
-    var asset = (item['fonts'] as List)[0]['asset'];
-    l[1] += '    - family: $family\n';
-    l[1] += '''      fonts:
-        - asset: $asset
-''';
+  final pubspecString = File(getAbsolutePath('pubspec.yaml')).readAsStringSync();
+  final pubspecContent = StringBuffer(pubspecString.replaceFirst(RegExp('flutter:.*\r?\n.*fonts:(\r|\n|.)*'), ''));
+
+  pubspecContent.writeln('flutter:');
+  pubspecContent.writeln('  fonts:');
+
+  for (final item in fontManifest) {
+    final family = item['family'];
+    final asset = (item['fonts'] as List)[0]['asset'];
+
+    pubspecContent.writeln('    - family: $family');
+    pubspecContent.writeln('      fonts:');
+    pubspecContent.writeln('        - asset: $asset');
   }
 
-  File(getAbsolutePath('pubspec.yaml')).writeAsStringSync(l.join('''flutter:
-  fonts:
-'''));
+  File(getAbsolutePath('pubspec.yaml')).writeAsStringSync(pubspecContent.toString());
 }
 
 void main() {
-  var vendorDir = './vendor';
+  final vendorDir = './vendor';
 
   // Copy fonts
-  runCommand(
-      'cp ${path.join(vendorDir, 'react-native-vector-icons/Fonts/*')} ${path.join(root, 'fonts')}');
+  runCommand('cp ${path.join(vendorDir, 'react-native-vector-icons/Fonts/*')} ${path.join(root, 'fonts')}');
 
   generateFonts();
   generateWebData();
